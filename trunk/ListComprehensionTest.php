bbe5fc371d15d31bbb24f3e3df1e4d01c0c4f633
vlad.andersen <vlad.andersen@2fad3f9b-9754-0410-b602-e7eb206d5adb>
2009-01-04 21:29:28 +0000

    Support for %-shortcut. Fix of the array bug.

--- trunk/ListComprehensionTest.php
+++ trunk/ListComprehensionTest.php
@@ -4,7 +4,7 @@ require_once 'ListComprehension.php';
 
 $errors = 0;
 
-/* Test 1 */
+/* Test 1. Basic example with numbers */
 
 $Foo = array (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 $Test = lc ('$i*2 for $i in $Foo if $i > 5', compact ('Foo'));
@@ -18,7 +18,7 @@ if ($Test != $Expected) {
 	$errors++;
 }
 
-/* Test 2 */
+/* Test 2. Basic example with strings */
 
 $Foo = array ('Alabama', 'California', 'Texas', 'New York');
 $Test = lc ('strtoupper($state) for $state in $Foo if strlen ($state) > 5 and strlen ($state) < 9', array ('Foo' => $Foo));
@@ -32,7 +32,7 @@ if ($Test != $Expected) {
   $errors++;
 }
 
-/* Test 3 */
+/* Test 3. Support for hash returns (with "{}") */
 
 $Foo = array ('AL' => 'Alabama', 'CA' => 'California', 'TE' => 'Texas', 'NY' => 'New York');
 $Test = lc ('{strtoupper($state) => strtolower ($code)} for $code => $state in $Foo if strlen ($state) > 5 and strlen ($state) < 9', array ('Foo' => $Foo));
@@ -46,7 +46,7 @@ if ($Test != $Expected) {
   $errors++;
 }
 
-/* Test 4 */
+/* Test 4. Support for hash shortcuts (without "{}") */
 
 $Foo = array (
   array ('name' => 'David Beckham', 'number' => 7),
@@ -64,6 +64,23 @@ if ($Test != $Expected) {
   $errors++;
 }
 
+/* Test 5. Support for %-shortcut. */
+
+$Foo = array (
+  array ('title' => 'Brad Pitt', 'id' => 1),
+  array ('title' => 'Al Pacino', 'id' => 2),
+  array ('title' => 'Keanu Reeves', 'id' => 3)
+);
+$Test = lc ('%<a href="/%s">%s</a> % $actor["id"], $actor["title"] for $actor in $Foo', array ('Foo' => $Foo));
+$Expected = array ('<a href="/1">Brad Pitt</a>', '<a href="/2">Al Pacino</a>', '<a href="/3">Keanu Reeves</a>');
+echo $Test == $Expected ? "Passed test 5\n" : "Failed test 5\n";
+if ($Test != $Expected) {
+  echo "\n--Expected:";
+  print_r ($Expected);
+  echo "\n--Result:";
+  print_r ($Test);
+  $errors++;
+}
 
 if (!$errors) {
 	echo "---\nAll tests passed, 0 errors.\n\n";
